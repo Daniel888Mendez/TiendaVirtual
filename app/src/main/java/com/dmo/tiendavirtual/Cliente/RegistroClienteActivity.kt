6659f3df7ed60bdcf19591cbfa867f38e002470e
Cajamar5 <>
2025-11-13 13:06:55 +0100

    Se ha creado la interface para registrar clientes

---
+++ app/src/main/java/com/dmo/tiendavirtual/Cliente/RegistroClienteActivity.kt
@@ -0,0 +1,16 @@
+package com.dmo.tiendavirtual.Cliente
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.dmo.tiendavirtual.R
+
+class RegistroClienteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro_cliente)
+
+    }
+}
