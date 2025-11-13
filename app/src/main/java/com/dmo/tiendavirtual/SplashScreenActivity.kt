0a2540a38323abe129c3bafd2bcd18254bd98a54
Cajamar5 <>
2025-11-13 12:00:05 +0100

    Se comprueba el tipo de usuario para lanzar activity de vendedor o de cliente

--- app/src/main/java/com/dmo/tiendavirtual/SplashScreenActivity.kt
+++ app/src/main/java/com/dmo/tiendavirtual/SplashScreenActivity.kt
@@ -4,12 +4,23 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.CountDownTimer
 import androidx.appcompat.app.AppCompatActivity
+import com.dmo.tiendavirtual.Cliente.MainActivityCliente
 import com.dmo.tiendavirtual.Vendedor.MainActivityVendedor
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class SplashScreenActivity : AppCompatActivity() {
+
+    private  lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        firebaseAuth= FirebaseAuth.getInstance()
 
         verBienvenida()
 
@@ -18,13 +29,38 @@ class SplashScreenActivity : AppCompatActivity() {
     private fun verBienvenida() {
         object : CountDownTimer(3000,1000){
             override fun onFinish() {
-                startActivity(Intent(applicationContext, MainActivityVendedor::class.java))
-                finishAffinity()
+                comprobarTipoUsuario()
             }
 
             override fun onTick(millisUntilFinished: Long) {
             }
 
         }.start()
+    }
+
+    private fun comprobarTipoUsuario(){
+        val usuario=firebaseAuth.currentUser
+        if (usuario==null){
+            startActivity(Intent(this, MainActivityVendedor::class.java))
+        }else{
+            val reference= FirebaseDatabase.getInstance().getReference("Usuarios")
+            reference.child(usuario.uid)
+                .addListenerForSingleValueEvent(object : ValueEventListener{
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                        val tipoU=snapshot.child("tipoUsuario").value
+                        if (tipoU=="Vendedor"){
+                            startActivity(Intent(this@SplashScreenActivity, MainActivityVendedor::class.java))
+                            finishAffinity()
+                        }else if (tipoU=="Cliente"){
+                            startActivity(Intent(this@SplashScreenActivity, MainActivityCliente::class.java))
+                            finishAffinity()
+                        }
+                    }
+
+                    override fun onCancelled(error: DatabaseError) {
+                    }
+
+                })
+        }
     }
 }
